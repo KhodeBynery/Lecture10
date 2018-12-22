d47f3742938ae0611972e39722f8d2340059a050
anonmape <anonmape@greenwood.com>
2018-12-21 22:39:54 -0400

    Lecture10

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
+// (function () {
+// 'use strict';
+//
+// angular.module('DIApp', [])
+// .controller('DIController', ['$scope', '$filter', DIController]);
+//
+// function DIController ($scope, $filter) {
+//   $scope.name = "Yaakov";
+//
+//   $scope.upper = function () {
+//     var upCase = $filter('uppercase');
+//     $scope.name = upCase($scope.name);
+//   };
+// }
+//
+// })();
+
+// (function () {
+// 'use strict';
+//
+// angular.module('DIApp', [])
+// .controller('DIController', ['$scope', '$filter', function ($scope,$filter) {
+//
+//   $scope.upper = function () {
+//     var upCase = $filter('uppercase');
+//     $scope.name = upCase($scope.name);
+//   }
+// }]);
+//
+// function DIController ($scope, $filter) {
+//   $scope.name = "Yaakov";
+//
+//   $scope.upper = function () {
+//     var upCase = $filter('uppercase');
+//     $scope.name = upCase($scope.name);
+//   };
+// }
+//
+// })();
+
 (function () {
 'use strict';
 
 angular.module('DIApp', [])
 .controller('DIController', DIController);
 
+DIController.$inject = ['$scope', '$filter'];
 function DIController ($scope, $filter) {
   $scope.name = "Yaakov";
 
